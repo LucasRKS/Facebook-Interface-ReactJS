c22b9a6bf1a11692600e0a010eebec602c1d95ef
Lucas Rogerio da Rosa Karger <lucasrogeriosk@gmail.com>
2020-01-07 16:58:44 -0300

    Estilizando Header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+
 import './Header.css';
 
 class Header extends Component {
   render() {
-    return <header><span id="facebook">Facebook</span></header>
+    return (<header>
+      <span id="facebook"><FontAwesomeIcon icon={faFacebook} /> facebook.</span>
+      <span id="perfil"><FontAwesomeIcon icon={faUser} /> Meu Perfil</span>
+    </header>)
   }
 }
 
